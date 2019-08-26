c7b6d206d32ac6d0863038528ad6808e9ae14a11
Huy Do <huyddo40@gmail.com>
2019-08-26 18:23:27 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,30 +23,31 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.find_by(:title => attributes[:title])
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # If no arguments are passed, use default values:
-  # title == "Home Alone"
-  # release_date == 1990
+  #  title == "Home Alone"
+  #  release_date == 1990
 
   Movie.create do |m|
-    __
+      m.title = args[:title]
+      m.release_date = args[:release_date]
   end
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  movie = Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  movie = Movie.last
 end
 
 def can_get_size_of_the_database
-  __
+    movie = Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+    movie = Movie.find(1)
 end
 
 def can_find_by_multiple_attributes
@@ -54,21 +55,24 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  movie = Movie.find_by(title:"Title",release_date:2000,director:"Me")
 end
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  __
+  movie = Movie.find_by_sql("SELECT * FROM movies
+  WHERE release_date > 2002
+  ORDER BY release_date desc")
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
-  Movie.create(title: "Awesome Flick")
-  __
-  __
-  __
+  movie = Movie.create(title: "Awesome Flick")
+  find_title =  movie.find_by(title: "Awesome Flick" )
+  movie.title = 'Even Awesomer Flick'
+  movie.save
+  movie
 end
 
 def can_update_using_update_method
