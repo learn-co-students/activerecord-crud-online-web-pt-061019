519396782f18b8975bb12144d7f043e58e1a21a9
Huy Do <huyddo40@gmail.com>
2019-08-26 20:40:29 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -84,7 +84,7 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  Movie.update(:title =>"A Movie")
+  Movie.update_all(:title =>"A Movie")
 end
 
 def can_destroy_a_single_item
