31b48d9563624936394039528c54604dfea1c338
David Foote <david.a.foote@gmail.com>
2019-08-28 19:46:42 +0000

    Done.

--- app/movie.rb
+++ app/movie.rb
@@ -91,8 +91,8 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  Movie.find(title: "That One Where the Guy Kicks Another Guy Once")
-  Movie.destroy
+  movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
+  movie.destroy 
 end
 
 def can_destroy_all_items_at_once
