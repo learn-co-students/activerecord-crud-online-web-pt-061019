18df0b01d7e98000a3ef10ff10292b6ea34c1c96
David Foote <david.a.foote@gmail.com>
2019-08-28 14:35:27 +0000

    Done.

--- db/migrate/20190828055730_create_movies.rb
+++
@@ -1,9 +0,0 @@
-class Movies < ActiveRecord::Migration[5.2]
-  def change
-    add_column :movies, :title, :string
-    add_column :movies, :release_date, :integer
-    add_column :movies, :director, :string
-    add_column :movies, :lead, :string
-    add_column :movies, :in_theaters, :boolean
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_08_28_141654) do
+
+  create_table "movies", force: :cascade do |t|
+  end
 
 end
