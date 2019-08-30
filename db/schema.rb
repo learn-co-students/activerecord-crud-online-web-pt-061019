4a7d42a9997cbd622f97cc50ea0e4a757f333403
Maasa <konomaasa@gmail.com>
2019-08-29 23:08:12 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2019_08_30_011005) do
 
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
+  end
+
 end
