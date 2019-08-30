087bc769755715bfe66e52d28058f64cb688c833
Maasa <konomaasa@gmail.com>
2019-08-29 19:21:44 -0700

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,7 +20,7 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie = Movie.new
 end
 
 def can_be_created_in_a_block(args = __)
