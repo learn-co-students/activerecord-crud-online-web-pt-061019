5a349846ecc0755996c312a8814565376959de8b
Oscar Shoudt <shoudto@gmail.com>
2019-09-10 20:13:36 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -92,7 +92,7 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  movie = Movie.find_by(title: "That One Where the guy Kicks Another Guy Once")
+  movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
   movie.destroy
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_195023) do
+ActiveRecord::Schema.define(version: 2019_09_10_185838) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
+  end
 
 end
